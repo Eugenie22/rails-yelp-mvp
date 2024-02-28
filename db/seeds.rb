ed68564ffb98f685713fd3d9b7bcaaffa302746b
Eugenier Elisa <theeugenieelisa@gmail.com>
2024-02-28 16:27:08 +0400

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'french' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'chinese' }
+pizza_west = { name: 'Pizza West', address: '57A Shoreditch High St, Chelsea E1 6PQ', category: 'italian' }
+kfc = { name: 'KFC', address: '57A Shoreditch High St, Manchester E1 6PQ', category: 'belgian' }
+dominos = { name: 'Dominos', address: '57A Shoreditch High St, Mauritius E1 6PQ', category: 'french' }
+
+[dishoom, pizza_east, pizza_west, kfc, dominos].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
